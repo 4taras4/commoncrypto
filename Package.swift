7f89aa428c2f0892f5cd006a7e74d20e0067261c
4taras4 <4taras4@gmail.com>
2020-06-03 00:41:22 +0300

    SPM

--- Package.swift
+++ Package.swift
@@ -3,7 +3,8 @@ import PackageDescription
 let package = Package(
     name: "CommonCrypto",
     targets: [
-        Target(name: "CommonCrypto"),
+        Target(name: "CommonCrypto",
+           path: "Sources"),
     ],
     dependencies: [
     	.Package(url: "https://github.com/kmussel/ccommoncrypto.git", "0.0.1"),
